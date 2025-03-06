3ea461e9689afdfa9d35461bde183faa557111df
nessim-cmd <bennasrnessim@outlook.com>
2025-03-06 13:44:19 +0100

    pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import Head from "next/head"; // Added for dynamic head tags
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
       appearance={{
         elements: {
           footer: {
-            display: "none", // Hides the footer entirely
+            display: "none",
           },
         },
       }}
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { NextConfig } from "next";
-// eslint-disable-next-line @typescript-eslint/no-require-imports
 const withPWA = require("next-pwa")({
-  dest: "public", // Where service worker files are generated
-  disable: process.env.NODE_ENV === "development", // Disable PWA in dev mode
-  register: true, // Auto-register the service worker
-  skipWaiting: true, // Activate new service worker immediately
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
 });
 
 const nextConfig: NextConfig = {
